a9a851b4755cfb1951325f12d0f3ac0aff000c2f
codersuresh <sd807141@gmail.com>
2023-06-04 18:29:40 +0545

    make fontsize of time smaller

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -27,7 +27,7 @@ const Card = () => {
                         <h2 className='font-semibold mt-4 text-xl'>Senior Frontend Developer</h2>
 
                         {/* card details */}
-                        <div className='flex items-center gap-x-4 mt-2'>
+                        <div className='flex text-sm items-center gap-x-4 mt-2'>
                             <p className='text-grayish-cyan'>1d ago</p>
                             <span className='h-1 w-1 bg-grayish-cyan rounded-full'></span>
                             <p className='text-grayish-cyan'>Full Time</p>
