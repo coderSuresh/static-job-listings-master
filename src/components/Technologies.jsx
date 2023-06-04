41eed59e476fc1e0109bc0d67b0034361667f126
codersuresh <sd807141@gmail.com>
2023-06-04 18:27:05 +0545

    add hove state in technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,15 +1,15 @@
 const Technologies = () => {
     return (
-        <div className='flex gap-4 flex-wrap mt-6 md:mt-0'>
-            <div className='rounded-md bg-light-cyan text-primary font-bold px-3 py-1'>Frontend</div>
-            <div className='rounded-md bg-light-cyan text-primary font-bold px-3 py-1'>Senior</div>
-            <div className='rounded-md bg-light-cyan text-primary font-bold px-3 py-1'>HTML</div>
-            <div className='rounded-md bg-light-cyan text-primary font-bold px-3 py-1'>Frontend</div>
-            <div className='rounded-md bg-light-cyan text-primary font-bold px-3 py-1'>Senior</div>
-            <div className='rounded-md bg-light-cyan text-primary font-bold px-3 py-1'>HTML</div>
-            <div className='rounded-md bg-light-cyan text-primary font-bold px-3 py-1'>Frontend</div>
-            <div className='rounded-md bg-light-cyan text-primary font-bold px-3 py-1'>Senior</div>
-            <div className='rounded-md bg-light-cyan text-primary font-bold px-3 py-1'>HTML</div>
+        <div className='flex md:justify-end gap-4 flex-wrap mt-6 md:mt-0'>
+            <button className='rounded-md hover:bg-primary hover:text-light-cyan bg-light-cyan text-primary font-bold px-3 py-1'>Frontend</button>
+            <button className='rounded-md hover:bg-primary hover:text-light-cyan bg-light-cyan text-primary font-bold px-3 py-1'>Senior</button>
+            <button className='rounded-md hover:bg-primary hover:text-light-cyan bg-light-cyan text-primary font-bold px-3 py-1'>HTML</button>
+            <button className='rounded-md hover:bg-primary hover:text-light-cyan bg-light-cyan text-primary font-bold px-3 py-1'>Frontend</button>
+            <button className='rounded-md hover:bg-primary hover:text-light-cyan bg-light-cyan text-primary font-bold px-3 py-1'>Senior</button>
+            <button className='rounded-md hover:bg-primary hover:text-light-cyan bg-light-cyan text-primary font-bold px-3 py-1'>HTML</button>
+            <button className='rounded-md hover:bg-primary hover:text-light-cyan bg-light-cyan text-primary font-bold px-3 py-1'>Frontend</button>
+            <button className='rounded-md hover:bg-primary hover:text-light-cyan bg-light-cyan text-primary font-bold px-3 py-1'>Senior</button>
+            <button className='rounded-md hover:bg-primary hover:text-light-cyan bg-light-cyan text-primary font-bold px-3 py-1'>HTML</button>
         </div>
     )
 }
