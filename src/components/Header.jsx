05e04b526ba44856f6065125a80a8effe0c500c6
codersuresh <sd807141@gmail.com>
2023-06-05 13:41:54 +0545

    increase header height

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <header className='md:h-20 md:mb-0 h-40 mb-4 bg-primary bg-hero-pattern-mobile md:bg-hero-pattern-desktop'>
+        <header className='md:h-28 md:mb-0 h-40 mb-4 bg-primary bg-hero-pattern-mobile md:bg-hero-pattern-desktop'>
         </header>
     )
 }
